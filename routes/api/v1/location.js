417f4be8d75af97a1e9ecb7c106f5c90cf77cf62
David Lopez Rodriguez <dloprodu@gmail.com>
2018-12-06 16:14:35 +0000

    add / remove locations

--- routes/api/v1/location.js
+++ routes/api/v1/location.js
@@ -5,7 +5,6 @@ const express = require('express')
   ,jwt = require('../../../lib/jwtAuth')
 
 const { check, validationResult } = require('express-validator/check');
-const Location = require('../../../models/Location');
 const User = require('../../../models/User');
 
 /**
@@ -24,16 +23,14 @@ router.post('/add', jwt(), [
   try {
     validationResult(req).throw();
     
-    const data = req.body
-    const newDataLocation = {
-      'user': req.userId,
+    const data = req.body;
+    const newLocation = {
+      'type': "Point",
       'description': data.description,
-      'location': {'type': "Point", "coordinates": [data.longitude, data.latitude]}
-    }
-    const newLocation = await new Location(newDataLocation)
-    const createLocation = await newLocation.save()
-    await User.findOneAndUpdate({_id: req.userId}, {$push: {locations: createLocation._id}})
-    res.ptcResponse()
+      'coordinates': [ data.longitude, data.latitude ]
+    };
+    await User.findOneAndUpdate({_id: req.userId}, { $push: { locations: newLocation }});
+    res.ptcResponse();
   } catch (err) {
     const error = new Error('LOCATION_ERROR_CREATE')
     return next(error);
@@ -44,18 +41,30 @@ router.post('/add', jwt(), [
  * POST /
  * Delete a location.
  * Query params:
- * - id: Required. Example: 5bfc1a6440dbed0d99ed5755 (id_location)
+ *  - description: Required. Example: Gimnasio People
+ *  - longitude: Required. Example : 40.783451 |-180 to 180|
+ *  - latitude: Required. Example: -1.234567 |-90 to 90|
 **/
 router.post('/delete', jwt(), [
-  check('id').isString().withMessage('ID_NECESSARY')
+  check('description').isString().isLength({min: 3, max: 1024}).withMessage('DESCRIPTION_NECESSARY'),
+  check('longitude').isFloat({min: -180, max: 180}).withMessage('COORDINATES_INVALID'),
+  check('latitude').isFloat({min: -90, max: 90}).withMessage('COORDINATES_INVALID')
 ], async (req, res, next) => {
   try {
     validationResult(req).throw()
     
     const data = req.body
-    const dataLocation = await Location.findOneAndRemove({_id: data.id})  
-    await User.findByIdAndUpdate({_id: req.userId}, {$pull: {locations: dataLocation._id}})
-    res.ptcResponse()
+    const location = {
+      'type': "Point",
+      'description': data.description,
+      'coordinates': [ data.longitude, data.latitude ]
+    };
+
+    await User.findByIdAndUpdate(
+      { _id: req.userId }, 
+      { $pull: { locations: location } }
+    );
+    res.ptcResponse();
   } catch (err) {
     const error = new Error('LOCATION_ERROR_DELETE')
     return next(error);
